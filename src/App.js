e136772347952e8c87cce5dc48a4e9fc6a956d45
Henry <duchenry2601@gmail.com>
2021-02-26 16:12:50 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { ChatEngine } from "react-chat-engine";
+import ChatFeed from "./components/ChatFeed.jsx"
 const App = () => {
   return (
     <ChatEngine
@@ -8,6 +9,7 @@ const App = () => {
   5606d523-4c47-419f-9318-ae1339cd3cf4"
       userName="henry"
       userSecret="43653661"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/>}
     />
   );
 };
