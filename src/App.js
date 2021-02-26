0f1fbf3490ba8f008b38ea7b2d98146afd6f5035
Henry <duchenry2601@gmail.com>
2021-02-26 13:32:48 +0700

    commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import { ChatEngine } from "react-chat-engine";
+const App = () => {
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="
+  5606d523-4c47-419f-9318-ae1339cd3cf4"
+      userName="henry"
+      userSecret="43653661"
+    />
+  );
+};
+
+export default App;
